5ca30165577fd9c60b2ac2315a0c1dd65739ecaf
Stepan <inin_01@mail.ru>
2024-09-10 16:20:01 +0700

    added class address

--- PetHelper.Backend/src/PetHelper.Domain/Models/Pet.cs
+++ PetHelper.Backend/src/PetHelper.Domain/Models/Pet.cs
@@ -9,7 +9,7 @@
         public string Breed { get; private set; } = null!;
         public string Color { get; private set; } = null!;
         public string HealthInformation { get; private set; } = null!;
-        public string Address { get; private set; } = null!;
+        public Address Address { get; private set; } = null!;
         public double Weight { get; private set; }
         public double Height { get; private set; }
         public string PhoneNumber { get; private set; } = null!;
@@ -17,7 +17,7 @@
         public DateOnly? DateOfBirth { get; private set; }
         public bool IsVaccinated { get; private set; }
         public StatusPet Status { get; private set; }
-        public List<DetailsForAssistance> DetailsForAssistance { get; private set; } = [];
-        public DateOnly CreatedDate { get; private set; }
+        public IReadOnlyList<DetailsForAssistance> DetailsForAssistance { get; private set; } = [];
+        public DateTime CreatedDate { get; private set; }
     }
 }
