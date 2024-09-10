7ea28d3970dda3e61e698bde4f26bdde52d59792
Stepan <inin_01@mail.ru>
2024-09-10 15:09:19 +0700

    add pet model

---
+++ PetHelper.Backend/src/PetHelper.Domain/Models/DetailsForAssistance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Domain.Models
+{
+    public class DetailsForAssistance
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ PetHelper.Backend/src/PetHelper.Domain/Models/StatusPet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHelper.Domain.Models
+{
+    public enum StatusPet
+    {
+        NeedsHelp,
+        LookingForHome,
+        FoundHome
+    }
+}
