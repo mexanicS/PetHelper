af280521b2d10c1942ff287293cb09e61d0c51df
Stepan <inin_01@mail.ru>
2024-09-28 18:46:00 +0700

    add serilog

--- PetHelper.Backend/src/PetHelper.API/Program.cs
+++ PetHelper.Backend/src/PetHelper.API/Program.cs
@@ -2,16 +2,36 @@ using FluentValidation.AspNetCore;
 using PetHelper.API.Validation;
 using PetHelper.Application;
 using PetHelper.Infastructure;
+using Serilog;
+using Serilog.Events;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.Debug()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq") 
+                 ?? throw new ArgumentNullException($"Seq"))
+    .Enrich.WithEnvironmentName()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentUserName()
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSerilog();
+
+builder.Services.AddHttpLogging(o =>
+{
+    o.CombineLogs = true;
+});
 
 builder.Services
     .AddApplication()
@@ -24,6 +44,8 @@ builder.Services.AddFluentValidationAutoValidation(configuration =>
 
 var app = builder.Build();
 
+
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -32,9 +54,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
